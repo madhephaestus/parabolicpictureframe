10b78219d377f3cdc6dc263d68f2af0e2ac0ac60
Kevin Harrington <mad.hephaestus@gmail.com>
2021-12-20 13:01:05 -0500

    adding glass

--- frame.groovy
+++ frame.groovy
@@ -1,7 +1,15 @@
 //Your code here
 def frameWidth =4*25.4
+def frameHeight =6*24.5
+
 def glassThickness = 6.5
 def baseHeight = 10
+
+def glass = new Cube(frameWidth,glassThickness,frameHeight).toCSG()
+	.toXMin()
+	.toZMin()
+	.movez(baseHeight)
+
 def base = Parabola.coneByHeight(20, baseHeight)
 			.rotx(90)
 			.toZMin()
@@ -15,8 +23,9 @@ def supportThickness = CSG.unionAll([
 					supportConnectCone.movey(glassThickness/2).movex(frameWidth),
 				     supportConnectCone.movey(glassThickness/-2).movex(frameWidth)])	
 				     .hull()
-return CSG.unionAll([
+glass.setColor(javafx.scene.paint.Color.WHITE);
+return [CSG.unionAll([
 base,
 base.movex(frameWidth),
 supportThickness
-])				   
+]),glass]			   
